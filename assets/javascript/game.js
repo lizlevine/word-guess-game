78ee141cebb8f30db679561994f842e3c69702ec
Tuscany <lizlevine.rigie@gmail.com>
2019-11-11 18:53:29 -0500

    added new functions and variables, refactored

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-var beginGame = document.getElementById;
+// var beginGame = document.getElementById;
 var userChoiceText = document.getElementById("userchoice-text");
 var computerChoiceText = document.getElementById("computerchoice-text");
 var winsText = document.getElementById("wins-text");
@@ -6,6 +6,7 @@ var lossesText = document.getElementById("losses-text");
 var underscoresContainer = document.getElementById("underscores");
 // var userGuess = document.getElementById("user-guess")["0"].value;
 
+// Array of Animals selected randomly
 var animalArray = [
   "cheetah",
   "zebra",
@@ -19,29 +20,48 @@ var animalArray = [
   "meerkat"
 ];
 
-// Stats:
+// Arrays and variables for holding data to be used inside of functions:
+
+var chosenAnimal = "";
+var blanksAndLetters = [];
+var blanks = 0;
+var lettersOfWord = [];
+var wrongGuesses = [];
+
+// possible outcomes
 var wins = 0;
 var losses = 0;
-var allowedGuesses = 7;
+var remainingGuesses = 0;
+var guessesAllowed = 11;
 
-function beginGame() {
-  allowedGuesses = 7;
-}
+// function beginGame() {
+//   guessesAllowed = 11;
+// }
 
 // var choice = math.floor(math.random() * animals.length);
 
 // function to get a random word from the array/ in own ecosystem doesn't care  about above
 // when you pass in arg. array, telling function what to do with it, right now a placeholder - can call it anything
-function randomWord(array) {
+function beginGame(array) {
   return array[Math.floor(Math.random() * array.length)];
+  lettersOfWord = chosenAnimal.split("");
+  blanks = lettersOfWord.length;
 }
+
+// function beginGame() {
+//   chosenAnimal = animalArray[Math.floor(Math.random() * Animalarray.length)];
+//   lettersOfWord = chosenAnimal.split("");
+// }
+
 // .random is the method.. whenever a funciton is inside an object we call it Math. object
 // return - function as a funnel... multiple inputs that can go into it and manipulate those
 // imputs and return one thing at the end. Return keyword allows you when you call to store
 // the output inside of a new variable - functions written in lowercase/camel case.
 // events (see)
 
-var chosenAnimal = randomWord(animalArray);
+var chosenAnimal = beginGame(animalArray);
+
+// beginGame();
 
 // could console log and see an animal name
 // return is usually the last line - no code after return line will run
@@ -56,6 +76,21 @@ function addUnderscores(chosenWord) {
     span.innerHTML = "_ ";
     underscoresContainer.appendChild(span);
   }
+
+  // refresh after every game
+  var remainingGuesses = 0;
+  var guessesAllowed = 11;
+  var wrongGuesses = [];
+  var blanksAndLetters = [];
+
+  // below are methods to update html
+
+  document.getElementById("guessed-so-far").innerHTML = blanksAndFilledSpaces;
+  document.getElementById;
+  document.getElementById;
+  document.getElementById;
+  document.getElementById;
+
   console.log(chosenWord);
   console.log(underscores);
   return underscores;
@@ -108,3 +143,14 @@ addUnderscores(chosenAnimal);
 //   document.getElementById("animalArray").innerHTML = output;
 //   output = "";
 // };
+// if we have all the correct letters...
+
+if (currentLetters.toString() === blanksAndFilledSpaces.toString()) {
+  wins++;
+  document.getElementById("");
+}
+
+document.getElementById("beginGame").onclick = function() {
+  buttons();
+};
+beginGame();
